8177c97372ca81e509f9d5d3a5d9d74f2e5d1988
Nathanstuff <n.odonnell@hotmail.com>
2021-02-20 15:23:28 +1100

    shopping cart has quantity of same item

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  // if exists, add 1 and return
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // if not exists, create with qty of 1
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
